3afd0e9b1ed610d74c80fc3ef81c4bad755cc80b
Henrik Hertler <henrik.hertler@gmail.com>
2023-02-28 19:36:37 +0100

    change text, add og image, change readme, remove stripe, img optim

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,18 @@
 import 'styles/main.css';
 import 'styles/chrome-bug.css';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Layout from 'components/Layout';
-import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { AppProps } from 'next/app';
 import { MyUserContextProvider } from 'utils/useUser';
-import type { Database } from 'types_db';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    const [supabaseClient] = useState(() => createBrowserSupabaseClient<Database>());
-    useEffect(() => {
-        document.body.classList?.remove('loading');
-    }, []);
-
     return (
         <div>
-            <SessionContextProvider supabaseClient={supabaseClient}>
-                <MyUserContextProvider>
-                    <Layout>
-                        <Component {...pageProps} />
-                    </Layout>
-                </MyUserContextProvider>
-            </SessionContextProvider>
+            <MyUserContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </MyUserContextProvider>
         </div>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,23 +22,32 @@ export default function IndexPage() {
             <div className='flex flex-col self-center p-6 bg-white content-center text-black text-center rounded md:mt-56 mt-40 md:max-w-md w-10/12 z-10'>
                 <p className='uppercase tracking-wider mb-4'>PUBLIC ANNOUNCEMENT</p>
                 <p>
-                    Hier entsteht der neue
-                    <br />
-                    Webauftritt des Hotel Centrals.
-                    <br />
-                    <br />
                     Das „Hotel Central“ ist ein digitaler und realer Ort mit dem Ziel, kreative Persönlichkeiten
-                    unterschiedlicher Disziplinen zu vernetzen und in ihrer Entwicklung zu unterstützen.
+                    unterschiedlicher Disziplinen zu vernetzen und in ihrer Entwicklung zu unterstützen. Unseren Gästen
+                    stellen wir Räumlichkeiten, Ausstattung sowie Veranstaltungs- und Netzwerkplattformen zur Verfügung,
+                    um ihr kreatives Arbeiten zu unterstützen. Wir heißen in unserem imaginären Hotel die
+                    unterschiedlichsten Menschen aus allen denkbaren, kreativen Bereichen willkommen. Gemeinsam
+                    verbringen sie Zeit in unserem Haus, diskutieren ihre Ideen, arbeiten an interdisziplinären
+                    Projekten mit Bezug zu Musik, Kunst, Fotografie, Design und Kultur und schaffen kreativen Output mit
+                    nachhaltigem und relevanten Stellenwert.
                     <br />
                     <br />
-                    Unseren Gästen stellen wir Räumlichkeiten, Ausstattung sowie Veranstaltungs- und Netzwerkplattformen
-                    zur Verfügung, um ihr kreatives Arbeiten zu unterstützen.
+                    JEDER GAST BRINGT SEINE GESCHICHTE UND SEIN TALENT MIT. SO ENTSTEHEN NEUE ETAGEN UND DAS HOTEL
+                    CENTRAL ERWACHT ZUM LEBEN.
                     <br />
                     <br />
-                    Wir heißen in unserem imaginären Hotel die unterschiedlichsten Menschen aus allen denkbaren
-                    kreativen Bereichen willkommen. Gemeinsam verbringen sie Zeit in unserem Haus, diskutieren ihre
-                    Ideen, arbeiten an interdisziplinären Projekten mit Bezug zu Musik, Kunst, Fotografie, Design und
-                    Kultur und schaffen kreativen Output mit nachhaltigem und relevanten Stellenwert.
+                    Der Austausch zwischen bekannten Professionals und jungen, aufstrebenden Kreativen ist ein wichtiger
+                    Bestandteil unserer Idee. Hierbei greifen wir auf unsere langjährige Erfahrung und Kenntnisse über
+                    die ansässige Subkultur zurück und bringen etablierte Protagonist:innen mit jungen Talenten
+                    zusammen. Wir glauben nicht nur daran, dass kreative Projekte besser werden, wenn verschiedene
+                    Menschen mit unterschiedlichem Background daran arbeiten. Wir glauben auch daran, dass die Welt eine
+                    bessere ist, wenn sich Menschen gegenseitig inspirieren, unterstützen und gemeinsam an der
+                    Realisierung arbeiten.
+                    <br />
+                    <br />
+                    <br />
+                    Hotel Central e.V. ist ein gemeinnütziger Verein, der alle dazu einlädt, Teil unserer Vision und
+                    Idee zu werden.
                 </p>
             </div>
 
